bb5f7450add67134e0a913694a7917b82ef88a6a
vagrant <vagrant@homestead>
2020-09-29 07:43:24 +0000

    Change: ユーザが契約したことがあるかないかで表示内容を変更

--- app/Http/Controllers/LessonsController.php
+++ app/Http/Controllers/LessonsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Lesson;
+use App\OrderDetail;
 use CreateUsersTable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,19 +17,33 @@ class LessonsController extends Controller
 
     //登録処理
     public function store(Request $request) {
-        $lesson = new Lesson();
-        $lesson->user_id = Auth::user()->id;
-        $lesson->title = $request->title;
-        $lesson->image = "image";
-        $lesson->keyword = $request->keyword;
-        $lesson->category_id = 1;
-        $lesson->body = $request->body;
-        $lesson->l_address = $request->l_address;
-        $lesson->m_address = $request->m_address;
-        $lesson->date = $request->date;
-        $lesson->time = $request->time;
-        $lesson->save();
-        return redirect()->route('lessons.show', ['lesson' => $lesson->id]);
+        $request->validate([
+            'title' => 'required|string|max:255', //入力必須、文字列、255文字以内
+            'category_id' => 'required',
+            // 'image' => 'file|image', //アップロードに成功している、画像ファイル
+            'body'=>'required|string|max:2048',
+            'l_address' => 'required',
+            'm_address' => 'required',
+            'date' => 'required',
+            'time' => 'required',
+        ]);
+
+            $lesson = new Lesson();
+            $lesson->user_id = Auth::user()->id;
+
+            $lesson->title = $request->title;
+            $lesson->image = 'image';
+            $lesson->keyword = $request->keyword;
+            $lesson->category_id = 1;
+            $lesson->body = $request->body;
+            $lesson->l_address = $request->l_address;
+            $lesson->m_address = $request->m_address;
+            $lesson->date = $request->date;
+            $lesson->time = $request->time;
+            //テーブルを保存
+            $lesson->save();
+            return redirect()->route('lessons.show', ['lesson' => $lesson->id])
+            ->with('flash_message', '正常に投稿が完了しました。');
     }
 
     //一覧画面
@@ -39,12 +54,15 @@ class LessonsController extends Controller
     //詳細画面
     public function show($id) {
         $lesson = Lesson::findOrFail($id);
-        return view('lessons.show', ['lesson' => $lesson]);
+        //$lessonを契約済か判断（order_details内にレコードがあればture:契約済、なければfalse:未契約）
+        $have_lesson = OrderDetail::where('user_id', Auth::user()->id)->where('lesson_id', $lesson->id)->exists();
+        return view('lessons.show', ['lesson' => $lesson, 'have_lesson' => $have_lesson]);
     }
 
     //編集画面
-    public function edit() {
-
+    public function edit($id) {
+        $lesson = Lesson::findOrFail($id);
+        return view('lessons.edit', ['lesson' => $lesson]);
     }
 
     //更新処理
--- resources/views/lessons/show.blade.php
+++ resources/views/lessons/show.blade.php
@@ -2,24 +2,52 @@
 
 @section('content')
 <div class="container">
-  <div class="row">
-    <div class="col-md-3">
-        @include('layouts.sidevar')
-    </div>
-    <div class="col-md-9">
-     <h3>レッスン詳細</h3>
-     <div class="card">
-        <div class="card-body">
-            <p>タイトル：{{ $lesson->title }}</p>
-            <p>キーワード：{{ $lesson->keyword }}</p>
-            <p>カテゴリー：{{ $lesson->category_id }}</p>
-            <p>開催日：{{ $lesson->date }}</p>
-            <p>開催時間：{{ $lesson->time }}</p>
-            <p>開催住所：{{ $lesson->l_address }}</p>
-            <p>レッスン内容：{{ $lesson->body }}</p>
+    <div class="row">
+        <div class="col-md-3">
+            @include('layouts.sidevar')
         </div>
-      </div>
+        <div class="col-md-9">
+        <h3>レッスン詳細</h3>
+            <div class="card">
+                <div class="card-body">
+                    <p>タイトル：{{ $lesson->title }}</p>
+                    <p>キーワード：{{ $lesson->keyword }}</p>
+                    <p>カテゴリー：{{ $lesson->category_id }}</p>
+                    <p>開催日：{{ $lesson->date }}</p>
+                    <p>開催時間：{{ $lesson->time }}</p>
+                    <p>開催住所：{{ $lesson->l_address }}{{ $lesson->m_address }}</p>
+                    <p>レッスン内容：{{ $lesson->body }}</p>
+                    <!-- レッスン作成者か判断 -->
+                    @if(Auth::user()->id == $lesson->user_id)
+                        <form method="get" action="{{ route('lessons.edit', [$lesson->id]) }}">
+                            {{ csrf_field() }}
+                            <input type="submit" value="編集する" class="btn btn-secondary btn-lg">
+                        </form>
+                        <form method="post" action="{{ route('lessons.destroy', [$lesson->id]) }}">
+                            {{ csrf_field() }}
+                            <input type="submit" value="レッスンを削除" class="btn btn-link" onclick='return confirm("{{ $lesson->title }}を本当に削除しますか？");'>
+                        </form>
+                    @else
+                        <!-- レッスン未購入か判断 -->
+                        @switch($have_lesson)
+                            @case(false)
+                                <form method="get" action="{{ route('lessons.confirm', [$lesson->id]) }}">
+                                    {{ csrf_field() }}
+                                    <input type="submit" value="契約する" class="btn btn-secondary btn-lg">
+                                </form>
+                                @break
+                            @case(true)
+                                <form method="get" action="">
+                                    {{ csrf_field() }}
+                                    <input type="submit" value="契約解除する" class="btn btn-secondary btn-lg">
+                                </form>
+                                @break
+                            @default
+                        @endswitch
+                    @endif
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 @endsection
